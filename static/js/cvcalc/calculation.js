e7e8eda0beee447087c0b4f4f3b9c0a67071b83a
Left <alfred.lui1@gmail.com>
2021-08-15 23:50:41 +1000

    Added hp scaling

--- static/js/cvcalc/calculation.js
+++ static/js/cvcalc/calculation.js
@@ -50,9 +50,19 @@ function multiplier(l) {
 	return str_factor(l) + dex_factor(l) + (stat_scaling([weaponData[weapon].dex]) * stat_scaling(bloodCodeData[bloodCode].dexterity)) + (stat_scaling(weaponData[weapon].str) * stat_scaling(bloodCodeData[bloodCode].strength));
 }
 
+function hp_stat_factor(stat) {
+	return letterReference[stat]/12;
+}
+
+function hp(l) {
+	return 5 * (l/(l+100/(hp_stat_factor(bloodCodeData[bloodCode].strength ) * 0.25 + hp_stat_factor(bloodCodeData[bloodCode].vitality));
+}
+
 function updateCalc() {
 	let base = weaponData[weapon].base * (1 + (0.1 * reinforcement));
+	let hp = 2000/3;
 	base = Math.floor(base * multiplier(level));
-	
+	hp = hp * (1 + hp(l));
+	$("#hpOutput").html(hp);
 	$("#damageOutput").html(base);
 }
