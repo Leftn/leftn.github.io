ae285c004c774354a3eb752bbef2509b24dd3832
Left <alfred.lui1@gmail.com>
2021-08-15 22:15:56 +1000

    Calculations done

--- static/js/cvcalc/calculation.js
+++ static/js/cvcalc/calculation.js
@@ -6,6 +6,24 @@ var reinforcement = "";
 var weaponData;
 var bloodCodeData;
 
+var letterReference = {
+	"S+":16,
+	"S":15,
+	"A+":14,
+	"A":13,
+	"B+":12,
+	"B":11,
+	"C+":10,
+	"C":9,
+	"D+":8,
+	"D":7,
+	"E+":6,
+	"E":5,
+	"-":0
+};
+
+
+
 $.getJSON("static/json/bloodcode.json", function(data) {
 	bloodCodeData = data;
 });
@@ -14,18 +32,28 @@ $.getJSON("static/json/weapon.json", function(data) {
 	weaponData = data;
 });
 
-function stat_factor(level) {
-	return Math.pow(level, 2);
+function stat_factor(stat) {
+	return Math.pow(letterReference[stat], 2);
 }
 
-function stat_scaling(level) {
-	return (Math.pow(level, 2/(100*Math.sqrt(5))));
+function stat_scaling(stat) {
+	return (Math.pow(letterReference[stat], 2/(100*Math.sqrt(5))));
 }
 
 function dex_factor(level) {
-	
+	stat_factor(weaponData[weapon].dexterity) * (Math.pow(level, 2) / (Math.pow(level, 2) + 10000/stat_factor(bloodCodeData[bloodCode].dexterity)));
+}
+
+function str_factor(level) {
+	stat_factor(weaponData[weapon].strength) * (Math.pow(level, 2) / (Math.pow(level, 2) + 10000/stat_factor(bloodCodeData[bloodCode].strength)));
+}
+
+function multiplier(level) {
+	return str_factor(level) + dex_factor(level) + (stat_scaling[weaponData[weapon].dexterity] * stat_scaling[bloodCodeData[bloodCode].dexterity]) + (stat_scaling[weaponData[weapon].strength] * stat_scaling[bloodCodeData[bloodCode].strength]);
 }
 
 function updateCalc() {
-	
+	let base = weaponData[weapon].base * (1 + (0.1 * reinforcement));
+	base = base * multiplier(level);
+	$("#damageOutput").html(base);
 }
