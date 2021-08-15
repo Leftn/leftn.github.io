c0266a6c5749bdebedd50011105c8f4d7de89b8b
Left <alfred.lui1@gmail.com>
2021-08-15 23:15:20 +1000

    Semicolons

--- static/js/cvcalc/cvinput.js
+++ static/js/cvcalc/cvinput.js
@@ -1,22 +1,22 @@
 document.getElementById("weaponReinforcement").addEventListener("change", function(e) {
 	$("#reinforcementFeedback").val("+"+e.target.value);
-	updateCalc()
+	updateCalc();
 });
 
 //weaponSelect
 document.getElementById("weaponSelect").addEventListener("change", function(e) {
 	weapon = e.target.value;
-	updateCalc()
+	updateCalc();
 });
 
 //bloodCodeSelect
 document.getElementById("bloodCodeSelect").addEventListener("change", function(e) {
 	bloodCode = e.target.value;
-	updateCalc()
+	updateCalc();
 });
 
 //levelInput
 document.getElementById("levelInput").addEventListener("change", function(e) {
-	level = e.target.value
-	updateCalc()
+	level = e.target.value;
+	updateCalc();
 });
