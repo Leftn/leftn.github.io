af106f97bc1a8fea5011b90742f1f0911d4c3ee6
Left <alfred.lui1@gmail.com>
2021-08-16 18:50:16 +1000

    Added stat boost scaling + stamina

--- static/js/cvcalc/calculation.js
+++ static/js/cvcalc/calculation.js
@@ -3,6 +3,12 @@ var weapon = "Zweihander";
 var level = "1";
 var reinforcement = "0";
 
+var strUp = 0;
+var dexUp = 0;
+var mndUp = 0;
+var vitUp = 0;
+var fortUp = 0;
+
 var weaponData;
 var bloodCodeData;
 
@@ -34,7 +40,6 @@ $("document").on("ready", function(e) {
 	updateCalc();
 });
 
-
 function stat_factor(stat) {
 	return Math.pow(letterReference[stat], 2)/100;
 }
@@ -44,30 +49,74 @@ function stat_scaling(stat) {
 }
 
 function dex_factor(l) {
-	return stat_factor(weaponData[weapon].dex) * (Math.pow(l, 2) / (Math.pow(l, 2) + 10000/stat_factor(bloodCodeData[bloodCode].dexterity)));
+	let n_dex;
+	if (l == 0) {
+		n_dex = dexUp;
+	} else {
+		n_dex = dexUp/l;
+	}
+	return stat_factor(weaponData[weapon].dex) * (Math.pow(l, 2) / (Math.pow(l, 2) + 10000/(stat_factor(bloodCodeData[bloodCode].dexterity) + n_dex)));
 }
 
 function str_factor(l) {
-	return stat_factor(weaponData[weapon].str) * (Math.pow(l, 2) / (Math.pow(l, 2) + 10000/stat_factor(bloodCodeData[bloodCode].strength)));
+	let n_str;
+	if (l == 0) {
+		n_str = strUp;
+	} else {
+		n_str = strUp/l;
+	}
+	return stat_factor(weaponData[weapon].str) * (Math.pow(l, 2) / (Math.pow(l, 2) + 10000/(stat_factor(bloodCodeData[bloodCode].strength) + n_str)));
 }
 
 function multiplier(l) {
-	return str_factor(l) + dex_factor(l) + (stat_scaling([weaponData[weapon].dex]) * stat_scaling(bloodCodeData[bloodCode].dexterity)) + (stat_scaling(weaponData[weapon].str) * stat_scaling(bloodCodeData[bloodCode].strength));
-}
-
-function hp_stat_factor(stat) {
-	return letterReference[stat]/12;
+	return str_factor(l) + dex_factor(l) + (stat_scaling([weaponData[weapon].dex]) * (Math.pow((letterReference[bloodCodeData[bloodCode].dexterity] + dexUp), 2/(100*Math.sqrt(5))))) + (stat_scaling(weaponData[weapon].str) * (Math.pow((letterReference[bloodCodeData[bloodCode].strength] + strUp), 2/(100*Math.sqrt(5)))));
 }
 
 function hp_multiplier(l) {
-	return 5 * (l/(l+100/(hp_stat_factor(bloodCodeData[bloodCode].strength ) * 0.25 + hp_stat_factor(bloodCodeData[bloodCode].vitality))));
+	let n_vit;
+	let n_str;
+	if (level == 0) {
+		n_vit = vitUp;
+		n_str = strUp;
+	} else {
+		n_vit = vitUp/level;
+		n_str = strUp/level;
+	}
+	return 5 * (l/(l+100/((letterReference[bloodCodeData[bloodCode].strength] + n_str)/12 * 0.25 + ((letterReference[bloodCodeData[bloodCode].vitality] + n_vit)/12)));
+}
+
+function flat_stamina_mind(L) {
+	let n_mnd;
+	if (level == 0) {
+		n_mnd = mndUp;
+	} else {
+		n_mnd = mndUp/parseInt(level)
+	}
+	return (3(L + n_mnd) - 40/3)
+}
+
+function flat_stamina_fortitude(L) {
+	let n_fort;
+	if (level == 0) {
+		n_fort = fortUp;
+	} else {
+		n_fort = fortUp/parseInt(level)
+	}
+	return (3(L + n_fort) - 40/3)
+}
+
+function stam_multiplier(l) {
+	return 100 * ((5/3 * l / (l + 100/((letterReference[bloodCodeData[bloodCode].mind]/15)/4 + letterReference[bloodCodeData[bloodCode].fortitude]/15))) + 1) + (1/4 * flat_stamina_mind(bc_mnd)+ flat_stamina_fortitude(bc_fort)))
 }
 
 function updateCalc() {
 	let base = weaponData[weapon].base * (1 + (0.1 * reinforcement));
 	let hp = 2000/3;
+	let stam;
 	base = Math.floor(base * multiplier(level));
 	hp = Math.ceil(hp * (1 + hp_multiplier(level)));
+	stam = Math.floor(stam_multiplier(level));
 	$("#hpOutput").html(hp);
 	$("#damageOutput").html(base);
+	$("#stamOutput").html(stam);
 }
--- static/js/cvcalc/cvinput.js
+++ static/js/cvcalc/cvinput.js
@@ -21,3 +21,29 @@ document.getElementById("levelInput").addEventListener("change", function(e) {
 	level = parseInt(e.target.value);
 	updateCalc();
 });
+
+// Stat boosts
+
+document.getElementById("StrUp").addEventListener("change", function(e) {
+	strUp = parseInt(e.target.value);
+	updateCalc();
+});
+
+document.getElementById("DexUp").addEventListener("change", function(e) {
+	dexUp = parseInt(e.target.value);
+	updateCalc();
+});
+document.getElementById("VitUp").addEventListener("change", function(e) {
+	vitUp = parseInt(e.target.value);
+	updateCalc();
+});
+
+document.getElementById("FortUp").addEventListener("change", function(e) {
+	fortUp = parseInt(e.target.value);
+	updateCalc();
+});
+
+document.getElementById("MndUp").addEventListener("change", function(e) {
+	mndUp = parseInt(e.target.value);
+	updateCalc();
+});
